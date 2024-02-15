425075fdc9a98d992a15cfde99166c8efd29d9a5
Koki Kazaore <koki.kazaore@smaregi.jp>
2024-02-15 12:45:11 +0900

    Create a nav link component

---
+++ src/app/components/NavLink.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+// NavLinkのプロパティの型定義
+interface NavLinkProps {
+    href: string;
+    title: string;
+}
+
+// FC（Function Component）を使用して、コンポーネントの型を定義
+const NavLink: React.FC<NavLinkProps> = ({ href, title }) => {
+    return (
+        <Link 
+            href={href} 
+            className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'
+        >
+            {title}
+        </Link>
+    );
+};
+
+export default NavLink;
