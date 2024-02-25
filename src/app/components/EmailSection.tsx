bf5b58bbba5c78f306d05ec47cabcdcd4fbb2e6b
Koki-Kazaore <kazaore@icloud.com>
2024-02-25 19:39:25 +0900

    delete unnecessary character "β"

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -64,7 +64,7 @@ const EmailSection: React.FC = () => {
                 <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
                 <p className='text-[#ADB7BE] mb-4 max-w-md'>
                     {" "}
-                    I&apos;m currently looking for new opportunities, my inbox is always openß.
+                    I&apos;m currently looking for new opportunities, my inbox is always open.
                     Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
                 </p>
                 <div className='socials flex flex-row gap-2'>
