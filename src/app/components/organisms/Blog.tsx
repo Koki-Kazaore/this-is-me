4477ea7f6d77e026db8cf8f4189efa2ffedf16e0
Koki-Kazaore <kazaore@icloud.com>
2024-10-05 16:52:57 +0900

    create organisms conmponent of blog

---
+++ src/app/components/organisms/Blog.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+import Navbar from './../Navbar'
+import Footer from '../Footer'
+import Article from '../molecules/Article'
+
+// Definition of article data
+const articles = [
+  {
+    id: 1,
+    title: 'Qiita Hackathon Participation Report',
+    content: 'This is a report on the participation in the Qiita Hackathon by team:m1nus. I will introduce the team members, the theme, the process, and the results of the hackathon.'
+  }
+]
+
+const Blog: FC = () => {
+  return (
+    <main className='flex flex-col min-h-screen bg-[rgb(18,18,18)]'>
+        <Navbar />
+        <div className='flex-grow sm:container mt-24 mx-auto px-12 py-4'>
+          {articles.map((article) => (
+            <Article key={article.id} title={article.title} content={article.content} />
+          ))}
+        </div>
+        <Footer />
+    </main>
+  )
+}
+
+export default Blog
