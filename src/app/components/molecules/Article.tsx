a96b8bf1017a5ca7680813c572d483cce8f0dc6d
koki-kazaore <kazaore@icloud.com>
2024-10-18 23:33:38 +0900

    Change the type of the id property of the Article component

--- src/app/components/molecules/Article.tsx
+++ src/app/components/molecules/Article.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import Link from 'next/link'
 
-const Article: FC<{ id: string, title: string; abstract: string }> = ({ id, title, abstract }) => (
+const Article: FC<{ id: number, title: string; abstract: string }> = ({ id, title, abstract }) => (
   <Link href={`/blog/${id}`}>
     <article className='text-white rounded-xl mt-3 bg-[#23272f] py-6 px-4'>
       <h2 className='text-2xl font-semibold mb-2'>{title}</h2>
