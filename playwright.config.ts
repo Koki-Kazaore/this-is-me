6514fea9a1d9cd58f7082967a1dd04752497b92b
Koki-Kazaore <kazaore@icloud.com>
2025-04-08 09:50:28 +0900

    Ensure that test case titles are output correctly

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['json', { outputFile: 'test-results.json' }]
+    ['json', { 
+      outputFile: 'test-results.json',
+      includeTitle: true
+    }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
